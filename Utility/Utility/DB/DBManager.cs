a4afc2e147b797704687ff43656d8f1a41b8c681
Luca Mori <mori.luca@hotmail.it>
2017-11-12 00:45:51 +0100

    creazione classe db manager

---
+++ Utility/Utility/DB/DBManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Utility.DB
+{
+    class DBManager
+    {
+        //Lock per la gestione del database
+        private object _lock;
+        
+
+        public DBManager()
+        {
+
+        }
+
+        //creazione istanza database se non esistente
+        public bool CreateDatabase()
+        {
+            lock (_lock)
+            {
+
+            }
+            return false;
+        }
+
+        //lettura di una lista
+        public bool lRead()
+        {
+            lock (_lock)
+            {
+
+            }
+            return false;
+        }
+
+        //scrittura di un nuovo record
+        public bool Write()
+        {
+            lock (_lock)
+            {
+
+            }
+            return false;
+        }
+    }
+}
