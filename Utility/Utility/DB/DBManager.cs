2595b98b9f5672d13df7e5f4f019fdb7fb7d88ef
Luca Mori <mori.luca@hotmail.it>
2017-11-12 02:24:27 +0100

    Implementazione classe di gestione connessione al database Creazione dei metodi lettura, scrittura e check connesisone

--- Utility/Utility/DB/DBManager.cs
+++ Utility/Utility/DB/DBManager.cs
@@ -11,41 +11,107 @@ namespace Utility.DB
     {
         //Lock per la gestione del database
         private object _lock;
-        
+        private bool _debugMode;
 
-        public DBManager()
+        //public properies
+        private string _databaseState;
+        public string DatabaseState { get => _databaseState; }
+
+
+        public DBManager(bool debugMode = false)
         {
-
+            _lock = new object();
+            _debugMode = debugMode;
+            CheckDBState();
         }
 
-        //creazione istanza database se non esistente
-        public bool CreateDatabase()
+        //Verifica stato del DB
+        public bool CheckDBState()
         {
             lock (_lock)
             {
-
+                try
+                {
+                    using (var context = new DBTest.lucaEntities())
+                    {
+                        if (context.Database.Exists())
+                        {
+                            context.Database.Connection.Open();
+                            context.Database.Connection.Close();
+                            int? timeOut = context.Database.CommandTimeout;
+                            _databaseState = "Database Found.\nConnection is availabe.\n";
+                            _databaseState += $"Server Name\t: {context.Database.Connection.DataSource}\n";
+                            _databaseState += $"Database Name\t: {context.Database.Connection.Database}\n";
+                            
+                            return true;
+                        }
+                        else
+                        {
+                            _databaseState = "Database Not Found!";
+                            return false;
+                        }
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Reading from the table wasnt possible
+                    //Display a message if debug mode is on
+                    if (_debugMode) Console.WriteLine(ex.Message);
+                    return false;
+                }
             }
-            return false;
+
         }
 
         //lettura di una lista
-        public bool lRead()
+        public bool ReadNomi(out List<DBTest.nomi> list)
         {
+
             lock (_lock)
             {
+                try
+                {
+                    using (var context = new DBTest.lucaEntities())
+                    {
+                        list = context.nome.ToList();
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    //Reading from the table wasnt possible
+                    //Display a message if debug mode is on
+                    if (_debugMode) Console.WriteLine(ex.Message);
+                    list = null;
+                    return false;
+                }
 
             }
-            return false;
         }
 
         //scrittura di un nuovo record
-        public bool Write()
+        public bool WriteOneName(string name)
         {
             lock (_lock)
             {
-
+                try
+                {
+                    using (var context = new DBTest.lucaEntities())
+                    {
+                        context.nome.Add(new DBTest.nomi { nome = name });
+                        context.SaveChanges();
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    //Reading from the table wasnt possible
+                    //Display a message if debug mode is on
+                    if (_debugMode) Console.WriteLine(ex.Message);
+                    return false;
+                }
             }
-            return false;
         }
     }
 }
--- Utility/Utility/Program.cs
+++ Utility/Utility/Program.cs
@@ -8,8 +8,23 @@ namespace Utility
 {
     class Program
     {
+        private static DB.DBManager _manager = new DB.DBManager(debugMode:true);
+        private static List<DBTest.nomi> _listaNomi = new List<DBTest.nomi>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine(_manager.DatabaseState);
+
+            //_manager.WriteOneName("farro");
+            if (_manager.ReadNomi(out _listaNomi))
+            {
+                foreach (var item in _listaNomi)
+                {
+                    Console.WriteLine(item.nome);
+                }
+            }
+
+            System.Console.ReadLine();
         }
     }
 }
