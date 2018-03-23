bec63b59ee97ecb241301d845c6b58ee0d42552e
luca mori <mori.luca@hotmail.it>
2018-03-23 23:25:13 +0100

    Nuova pertenza del progetto, eliminati  i vecchi e inseriti i nuovi email logger e omron serviceOpener

---
+++ Utility/Email/Interfaces/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Luca
+{
+    public interface IEmailService
+    {
+        void Send(EmailMessage emailMessage);
+        List<EmailMessage> ReceiveEmail(int maxCount = 10);
+    }
+}
