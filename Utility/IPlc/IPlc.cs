ef10be6012c303c2006656e16a7beda9b85f2e70
luca mori <mori.luca@hotmail.it>
2018-04-25 17:24:46 +0200

    Creazione interfaccia plc e inizio implementazione classe plc siemens

---
+++ Utility/IPlc/IPlc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace IPlc
+{
+    interface IPlc
+    {
+        #region Properties
+        /// <summary>
+        /// Return the state of the connection with the PLC
+        /// </summary>
+        bool Connecetd { get; set; }
+
+        /// <summary>
+        /// Return where the plc is available for comunication
+        /// </summary>
+        bool Available { get; set; }
+
+        /// <summary>
+        /// return the plc name
+        /// </summary>
+        string PlcName { get; set; }
+
+        /// <summary>
+        /// Ip address of the plc
+        /// </summary>
+        IPAddress iPAddress { get; set; }
+
+        /// <summary>
+        /// Port for plc comunication, if necessary
+        /// </summary>
+        int Port { get; set; }
+
+        /// <summary>
+        /// Plc Node, if necessary, for example for Fins Networks
+        /// </summary>
+        int Node { get; set; }
+        #endregion
+
+        #region Connect/Disconnect
+        /// <summary>
+        /// Open the connection
+        /// </summary>
+        /// <returns>True if plc is now connected</returns>
+        bool OpenConnetion();
+
+        /// <summary>
+        /// Close the comunication
+        /// </summary>
+        /// <returns>True if the comunication is now closed</returns>
+        bool CloseConnection();
+        #endregion
+
+        #region Comunication With PLC
+        bool? ReadBool(int Area, int MemoryAddress, int offset);
+        char? ReadChar(int Area, int MemoryAddress, int offset);
+        Int16? ReadInt16(int Area, int MemoryAddress, int offset);
+        UInt16? ReadU_Int16(int Area, int MemoryAddress, int offset);
+
+        Int32? ReadInt32(int Area, int MemoryAddress, int offset);
+        UInt32? ReadU_Int32(int Area, int MemoryAddress, int offset);
+
+        Int64? ReadInt64(int Area, int MemoryAddress, int offset);
+        UInt64? ReadU_Int64(int Area, int MemoryAddress, int offset);
+
+        byte ReadByte(int Area, int MemoryAddress, int offset);
+        byte[] ReadWord(int Area, int MemoryAddress, int offset);
+        byte[] ReadDoubleWord(int Area, int MemoryAddress, int offset);
+        byte[] ReadByteArray(int Area, int MemoryAddress, int offset, int Count);
+
+        string ReadString(int Area, int MemoryAddress, int offset);
+        #endregion
+
+        #region Recive Command From Queue
+        bool OnCommandRecived();
+        #endregion
+
+        #region Send Answer To Queue
+        bool AnswerBack();
+        #endregion
+    }
+}
---
+++ Utility/SiemensPLC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SiemensPLC
+{
+    public class Class1
+    {
+    }
+}
